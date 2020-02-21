dbceb31a1f5e8dbf0930c510cb1c87f456199603
asaliva <csi17-asaliva@colegioaltair.net>
2020-02-21 09:22:30 +0100

    login añadido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',
+  loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+  {path: '',
+    loadChildren: () => import('./horario/horario.module').then(m => m.HorarioModule)},
+  {path: '',
+    loadChildren: () => import('./profesores/profesores.module').then(m => m.ProfesoresModule)},
+  {path: '',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
+  {path: '',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
